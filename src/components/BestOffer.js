e7fd6eb431c0b4e11a454dc3740a52fdf22997a4
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-10-24 21:05:34 +0200

    logic caourosel

--- src/components/BestOffer.js
+++ src/components/BestOffer.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 
 const BestOffer = () => {
 
-    const data =  [{
+    const data =  [
+        {
         image: "https://m.atcdn.co.uk/a/media/w800h600/0ca14354a2d340dcb1a989ba871ec80d.jpg",
         name: "Peugeot 308 SW",
         price: "24,746",
@@ -24,11 +25,17 @@ const BestOffer = () => {
 
 
       const nextBestOffer = () => {
-        setBestOfferIndex(bestOfferIndex++)
+        const endArray = bestOffer.length;
+        const next = bestOfferIndex+1 === endArray ? 0 : bestOfferIndex+1;
+        setBestOfferIndex(next)
+        
       }
       
       const previousBestOffer = () => {
-        setBestOfferIndex(bestOfferIndex--)
+        const endArray = bestOffer.length;
+        const prev = bestOfferIndex <= 0 ? endArray-1 : bestOfferIndex-1;
+        console.log(prev, endArray,bestOfferIndex )
+        setBestOfferIndex(prev)
       }
 
 // const server = "" // this will come from redux or contest
@@ -39,51 +46,31 @@ const BestOffer = () => {
 //         .catch(err => console.log(err))
 // }, [])
 
-
+      console.log(bestOfferIndex, bestOffer)
 
   return (
 
-
-
-<div id="indicators-carousel" class="relative w-2/4 m-auto" data-carousel="static">
-    {/* <!-- Carousel wrapper --> */}
-    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-         {/* <!-- Item 1 --> */}
+    <div className='w-3/4 flex flex-col align-center justify-center m-auto'>
        
-       <div className="duration-700 ease-in-out absolute inset-0 transition-all transform translate-x-0 z-20" data-carousel-item>
         
             <h1 className='text-center'>{bestOffer[bestOfferIndex].name}</h1>
-            <img src={bestOffer.image} alt={bestOffer[bestOfferIndex].name} className='w-1/4 absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2' />
-            <p className="pt-60">{bestOffer[bestOfferIndex].description}</p>
-        </div>  
-       
-        
+            <img src={bestOffer[bestOfferIndex].image} alt={bestOffer[bestOfferIndex].name} className='w-fit h-fit' />
+            <p className="">{bestOffer[bestOfferIndex].description}</p>
+
+            <div className='flex justify-between'>
+                    <button onClick={ (e) => previousBestOffer()}>
+                        <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" ><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" ></path></svg>
+                        
+                    </button>
+
+                    <button onClick={ (e) => nextBestOffer()}>
+                        <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" ></path></svg>
+
+                    </button>
+
+            </div>
 
     </div>
-  
-    {/* <!-- Slider indicators --> */}
-    <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-        <button type="button" class="w-3 h-3 rounded-full bg-white dark:bg-gray-800" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-        <button type="button" class="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-        <button type="button" class="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-        <button type="button" class="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-        <button type="button" class="w-3 h-3 rounded-full bg-white/50 dark:bg-gray-800/50 hover:bg-white dark:hover:bg-gray-800" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-    </div>
-    
-    {/* <!-- Slider controls --> */}
-    <button type="button" className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev="">
-        <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path></svg>
-            <span class="sr-only">Previous</span>
-        </span>
-    </button>
-    <button type="button" className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next="">
-        <span class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path></svg>
-            <span class="sr-only">Next</span>
-        </span>
-    </button>
-</div>
 
 
 
