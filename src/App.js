fa512e07d4a259e82c4eb32e8472f611a2c03231
Bidz42 <abid97r@gmail.com>
2022-10-24 12:46:27 +0100

    navbar and footer done with page layouts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Route, Routes} from 'react-router-dom'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import CarDetails from './pages/CarDetails'
+import Cars from './pages/Cars'
+import Info from './pages/Info'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='flex flex-col h-screen justify-between'>
+    <div>
+        <Navbar />
+    </div>
+       <Routes>
+          <Route path="/" element={<Cars />} />
+          <Route path="/:id" element={<CarDetails />} />
+          <Route path="/info" element={<Info />} />
+          <Route path="/admin/login" element={<Login />} />
+       </Routes>
+    <div>
+      <Footer />
+    </div>
+    </div>
   )
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+
+ const [navbarOpen, setNavbarOpen] = useState(false);
+ 
+  return (
+    <>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-emerald-500 mb-3">
+        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+          <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+            <Link
+              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+              to="/"
+            >
+              Buy A Car
+            </Link>
+            <button
+              className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+              type="button"
+              onClick={() => setNavbarOpen(!navbarOpen)}
+            >
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="text-white w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            </svg>
+            </button>
+          </div>
+          <div
+            className={
+              "lg:flex flex-grow items-center" +
+              (navbarOpen ? " flex" : " hidden")
+            }
+            id="example-navbar-danger"
+          >
+            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+              <li className="nav-item">
+                <Link
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  to="/"
+                >
+                  <span className="ml-2">All Cars</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                  to="/info"
+                >
+                  <span className="ml-2">Info</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
