c5f54f40428cf83823219e0fe63beb324452a60f
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-10-29 01:44:03 +0200

    working on responsive layout for testimonial

--- src/components/Testimonial.js
+++ src/components/Testimonial.js
@@ -15,10 +15,15 @@ const Testimonial = () => {
   return (
     <div className='text-white h-screen mt-60'>
 
-    <h1 className='text-lg md:text-4xl text-center m-auto font-bold text-gold mb-10'>Témoignages </h1>
+    <h1 className='text-lg md:text-4xl text-center font-bold text-gold mb-10'>Témoignages </h1>
+    
+    <section className='flex flex-row md:w-1/2 m-auto'>
+
+    
+    <svg aria-hidden="true" className="w-5 h-5 text-gold  w-1/6  my-auto " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" ><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" ></path></svg> 
     {testimonial.map(item => 
 
-    <section key={item._id} className="flex flex-col w-1/2 m-auto">
+    <section key={item._id} className="flex flex-col w-4/6 m-auto">
     
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className='fill-gold w-1/6'>
         <path d="M0 216C0 149.7 53.7 96 120 96h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V320 288 216zm256 0c0-66.3 53.7-120 120-120h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H320c-35.3 0-64-28.7-64-64V320 288 216z"/>
@@ -30,11 +35,13 @@ const Testimonial = () => {
         <aside className='flex justify-end'>
             <h2 className='text-2xl'>- {item.author}</h2>
         </aside>
-
     </section>
     )
     }
+    
+    <svg aria-hidden="true" className="w-5 h-5 text-gold my-auto  w-1/6   " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" ></path></svg>
 
+    </section>
     
     </div>
   )
