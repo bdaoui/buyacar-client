e99b0efe63dba4ccaa1f2a7650b5acc016d50312
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-10-29 12:48:36 +0200

    working on specific page

--- src/pages/CarDetails.js
+++ src/pages/CarDetails.js
@@ -19,6 +19,23 @@ const CarDetails = () => {
   }, []);
 
 
+  const [ImageIndex, setImageIndex] = useState(0)
+
+
+
+  const nextImage = () => {
+    const endArray = car.length;
+    const next = ImageIndex+1 === endArray ? 0 : ImageIndex+1;
+    setImageIndex(next)
+    
+  }
+  
+  const previousImage = () => {
+    const endArray = car.length;
+    const prev = ImageIndex <= 0 ? endArray-1 : ImageIndex-1;
+    console.log(prev, endArray,ImageIndex )
+    setImageIndex(prev)
+  }
 
   return (
     <div className='text-white h-full w-full'>
@@ -29,32 +46,40 @@ const CarDetails = () => {
       <section className='flex flex-col lg:flex-row w-screen h-full'>
       
           <div className='w-full lg:w-2/4'>  
-          
+          <button onClick={ (e) => previousImage()} className="pr-2" >
+                        <svg aria-hidden="true" className="w-5 h-5 text-gold " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" ><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" ></path></svg>    
+          </button>
           {car?.image?.map((i, index) => {
             return <div key={index}>
+            
             <img src={i} alt={i?.make} className='flex justify-center mx-auto mb-10 md:mb-20 px-10' />
             </div>
           })}
+
+          <button onClick={ (e) => nextImage()} className="pl-2">
+                        <svg aria-hidden="true" className="w-5 h-5 text-gold" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" ></path></svg>
+          </button>
           </div>
 
-          <div className='w-full lg:w-2/4 h-screen text-base flex flex-col lg:flex-row gap-5 lg:gap-20'>
-            <section className='flex flex-col gap-y-5 lg:gap-y-10 text-center lg:text-left'>
+          <div className='w-full lg:w-3/4 h-screen text-base flex flex-col lg:flex-row gap-5 lg:gap-10'>
+            <section className='flex md:w-1/4 flex-col gap-y-5 lg:gap-y-10 text-center lg:text-left'>
               <p>Marque: <span className='italic text-gold'>{car?.make}</span></p>
               <p>Modèle: <span className='italic text-gold'>{car?.model}</span></p>
-              <p>€ <span className='italic text-gold'>{car?.price}</span></p>
+              <p>Prix: <span className='italic text-gold'>{car?.price +"€"}</span></p>
               <p>Kilométrage: <span className='italic text-gold'>{car?.mileage}</span></p>
               <p>Couleurs: <span className='italic text-gold'>{car?.color}</span></p>
               <p>Carburant: <span className='italic text-gold'>{car?.fuel}</span></p>
             </section>
 
-            <section className='flex flex-col gap-y-5 lg:gap-y-10 text-center lg:text-left'>
+            <section className='flex md:w-1/4 flex-col gap-y-5 lg:gap-y-10 text-center lg:text-left'>
               <p>Boîte de vitesse: <span className='italic text-gold'>{car?.transmission}</span></p>
-              <p>Engine Size: <span className='italic text-gold'>{car?.engine}L</span></p>
-              <p>Doors: <span className='italic text-gold'>{car?.doors}</span></p>
-              <p>Seats: <span className='italic text-gold'>{car?.seats}</span></p>
+              <p>Moteur: <span className='italic text-gold'>{car?.engine}L</span></p>
+              <p>Nombre de portes: <span className='italic text-gold'>{car?.doors}</span></p>
+              <p>Nombre de place(s): <span className='italic text-gold'>{car?.seats}</span></p>
+              <p>Année-modèle: <span className='italic text-gold'>{car?.seats}</span></p>
             </section>
 
-            <section className='flex flex-col h-full gap-y-5 lg:gap-y-10 text-center mx-20 lg:text-left'>
+            <section className='flex md:w-2/4 flex-col h-full gap-y-5 lg:gap-y-10 text-center mx-20 lg:text-left'>
               Description: <span className='italic text-gold'>{car?.description}</span>
             </section>
 
