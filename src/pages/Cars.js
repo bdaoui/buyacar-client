b83d350e36ee5f2c7dfdb8b293b5c5cbf4c8fede
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-11-02 20:32:43 +0100

    arrow in cars list

--- src/pages/Cars.js
+++ src/pages/Cars.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Filter from "../components/Filter";
+import { HashLink } from "react-router-hash-link";
 
 const Cars = () => {
   const server = "http://localhost:5005";
@@ -90,8 +91,51 @@ const Cars = () => {
     setFilteredCars(cars)
   }
 
+
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const listenToScroll = () => {
+    let hideTill = 200;
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    if (winScroll > hideTill) {
+      isVisible && setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
   return (
     <div>
+
+
+{isVisible && (
+      <HashLink smooth to="/cars#top">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          className="w-8 h-8 mb-1 md:mb-0 md:w-10 md:h-10 bottom-16 right-1 md:bottom-10 md:right-1 lg:bottom-20 lg:right-10 fixed text-gold"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </HashLink>
+    )}
+
+
       <div
         id="carGallery"
         className="flex flex-col md:flex-row justify-center md:w-2/4 m-auto md:flex-wrap p-10"
