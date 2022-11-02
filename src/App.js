64cfea490ae0d6c9d6ddd314330040347ad2f302
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-11-02 20:29:32 +0100

    fixing layout bestOffer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import { HashLink } from 'react-router-hash-link';
 import { Route, Routes} from 'react-router-dom'
 import CarDetails from './pages/CarDetails'
 import Landing from './pages/Landing'
@@ -10,46 +9,11 @@ import Dashboard from './pages/Dashboard'
 import OutletComponent from './components/OutletComponent'
 
 const App = () => {
-  const [isVisible, setIsVisible] = useState(true);
 
-  useEffect(() => {
-    window.addEventListener("scroll", listenToScroll);
-    return () => window.removeEventListener("scroll", listenToScroll);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const listenToScroll = () => {
-    let hideTill = 200;
-    const winScroll =
-      document.body.scrollTop || document.documentElement.scrollTop;
-
-    if (winScroll > hideTill) {
-      isVisible && setIsVisible(true);
-    } else {
-      setIsVisible(false);
-    }
-  };
 
   return (
     <div className='App h-100vh bg-black/90'>
-      {isVisible && (
-        <HashLink smooth to="/#top">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            stroke="currentColor"
-            className="w-8 h-8 mb-1 md:mb-0 md:w-10 md:h-10 bottom-16 right-1 md:bottom-10 md:right-1 lg:bottom-20 lg:right-10 fixed text-gold"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-            />
-          </svg>
-        </HashLink>
-      )}
+ 
   
        <Routes>
           <Route element={<OutletComponent />}>
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -7,6 +7,9 @@ import { Link } from "react-router-dom";
 import Testimonial from "../components/Testimonial";
 import axios from "axios";
 import ContactForm from "../components/ContactForm";
+import { HashLink } from "react-router-hash-link";
+
+
 
 const Landing = () => {
   const [cars, setCars] = useState([]);
@@ -19,10 +22,59 @@ const Landing = () => {
       .catch((err) => console.log(err));
   }, []);
 
+
+
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const listenToScroll = () => {
+    let hideTill = 200;
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    if (winScroll > hideTill) {
+      isVisible && setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+
+  
+  
+  
+  
+  
   return (
     <div>
+    {isVisible && (
+      <HashLink smooth to="/#top">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="2"
+          stroke="currentColor"
+          className="w-8 h-8 mb-1 md:mb-0 md:w-10 md:h-10 bottom-16 right-1 md:bottom-10 md:right-1 lg:bottom-20 lg:right-10 fixed text-gold"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 11.25l-3-3m0 0l-3 3m3-3v7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </HashLink>
+    )}
       <Header />
-      <BestOffer />
+
+      <section className="bg-black">
+        <BestOffer />
+      </section>
 
       <div
         id="carGallery"
