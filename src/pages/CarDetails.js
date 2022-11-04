d5d70dfe5007db8adec78051d341aa1bf7654780
Bidz42 <abid97r@gmail.com>
2022-11-04 19:33:44 +0000

    sm and md car detail page done but lg needs to be done, also xs responsiveness needed

--- src/pages/CarDetails.js
+++ src/pages/CarDetails.js
@@ -1,13 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import {BsCurrencyEuro, BsFillCalendar2DateFill} from 'react-icons/bs'
-import {FaRoad} from 'react-icons/fa'
-import {TfiSpray} from 'react-icons/tfi'
-import {RiGasStationFill} from 'react-icons/ri'
-import {GiGearStickPattern, GiCarDoor, GiCarSeat} from 'react-icons/gi'
-import {TbEngine} from 'react-icons/tb'
-import {CgQuote} from 'react-icons/cg'
+import CarTextSM from "../components/singleCar/CarTextSM";
+import CarTextLG from "../components/singleCar/CarTextLG";
 
 const CarDetails = () => {
   const { id } = useParams();
@@ -99,71 +94,12 @@ const CarDetails = () => {
         </div>
 </section>
 
-
-        <div className="w-full text-xs md:text-xl flex">
-          
-       
-          <section className="flex flex-col w-1/2 gap-y-2 md:gap-y-5">
-
-            <p className="flex justify-left pl-10 md:pl-32">
-              Marque: <span className="italic text-gold ml-2">{car?.make}</span>
-            </p>
-
-            <p className="flex justify-left pl-10 md:pl-32">
-              Modèle: <span className="italic text-gold ml-2">{car?.model}</span>
-            </p>
-           
-              <p className="flex justify-left pl-10 md:pl-32"><BsCurrencyEuro className="mt-1 mr-2 text-lg"/> 
-              <span className="italic text-gold">{car?.price}</span></p>
-            
-              <p className="flex justify-left pl-10 md:pl-32">
-              <FaRoad className="mt-1 mr-2 text-lg" />
-              <span className="italic text-gold">{car?.mileage}km</span>
-            </p>
-          
-            <p className="flex justify-left pl-10 md:pl-32">
-              <TfiSpray className="mt-1 mr-2 text-lg"/> <span className="italic text-gold">{car?.color}</span>
-            </p>
-
-            <p className="flex justify-left pl-10 md:pl-32">
-              <RiGasStationFill className="mt-1 mr-2 text-lg"/> <span className="italic text-gold">{car?.fuel}</span>
-            </p>
-         
-</section>
-<section className="flex flex-col w-1/2 gap-y-2 md:gap-y-5">
-          <p className="flex justify-left pl-10 md:pl-32">
-              <GiGearStickPattern className="mt-1 mr-2 text-lg"/>
-              <span className="italic text-gold">{car?.transmission}</span>
-            </p>
-
-            <p className="flex justify-left pl-10 md:pl-32">
-              <TbEngine className="mt-1 mr-2 text-lg"/> <span className="italic text-gold">{car?.engine} L</span>
-            </p>
-           
-            <p className="flex justify-left pl-10 md:pl-32">
-              <GiCarDoor className="mt-1 mr-2 text-lg"/>
-              <span className="italic text-gold">{car?.doors}</span>
-            </p>
-
-            <p className="flex justify-left pl-10 md:pl-32">
-              <GiCarSeat className="mt-1 mr-2 text-lg"/>
-              <span className="italic text-gold">{car?.seats}</span>
-            </p>
-
-            <p className="flex justify-left pl-10 md:pl-32">
-              <BsFillCalendar2DateFill className="mt-1 mr-2 text-lg"/>
-              <span className="italic text-gold">{car?.year}</span>
-            </p>
-            
-
-          </section>
-        </div>
-<section className="flex flex-col w-full py-5 pb-20">
-       <CgQuote className="mt-3 text-5xl ml-5"/>
-          <p className="text-xs md:text-base px-10">
-            <span className="italic text-gold ">{car?.description}</span></p>
-
-            </section>
+<div className="lg:hidden">
+       <CarTextSM car={car} />
+</div>
+<div className="hidden lg:block">
+        <CarTextLG car={car} />
+</div>
 
       
     </div>
