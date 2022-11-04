7389e56f3a79c555a6cf2c71c4baa0fe25525af9
Bidz42 <abid97r@gmail.com>
2022-11-04 19:27:44 +0000

    sm and md car detail page done but lg needs to be done, also xs responsiveness needed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => {
 
 
   return (
-    <div className='App h-100vh bg-black/90'>
+    <div className='App bg-black/90'>
   
        <Routes>
           <Route element={<OutletComponent />}>
